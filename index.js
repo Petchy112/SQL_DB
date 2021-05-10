4f090985cf5326cf356ae088983a0ed9fda78555
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-05-10 18:47:03 +0700

    Create DB

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const mysql = require('mysql');
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000;
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+const port = process.env.port || 4000
+const connection = mysql.createConnection({
+  host:'localhost',
+  user: '',
+  password: ''
 });
-
-con.connect(function(err) {
+connection.connect(function(err) {
   if (err) return err;
-  console.log("Connected!");
+  console.log('Connected')
+})
+
+app.get('/',(req,res) => {
+  connection.query('CREATE DATABASE mysql',(err,result) => {
+      res.send(result);
+  })
 });
 
-app.listen(port,() => {
-    console.log('connect seccess')
-})
+app.get('/',(req,res) => res.send('Hello SQL'))
+
+app.listen(port , () => console.log(`App listening at port ${port}`))
